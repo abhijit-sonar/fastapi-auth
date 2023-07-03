0be1b339c615b840d7f694b517e3baf7dd3158c0
Abhijit Sonar <abhijit.sonar@thinkitive.com>
2023-07-03 17:20:25 +0530

    CORS: Allow all origins

--- people_api/__init__.py
+++ people_api/__init__.py
@@ -1,7 +1,19 @@
+from fastapi.middleware import Middleware
+from typing import Any
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import routes
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(routes.router)
